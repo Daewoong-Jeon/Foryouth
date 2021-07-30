0db0a45f9a749e1a6d647e0d4ccdad8bbec7f4dd
djeon <du0928@naver.com>
2021-07-30 21:02:07 +0900

    add drawing table function2

--- getapi.js
+++ getapi.js
@@ -1,9 +1,17 @@
+// 엄격한 js 문법검사
+'use strict'
+// 현재 날짜
 let today = new Date();
 var today_date = today.getFullYear() + '-' + today.getMonth() + '-' + today.getDate();
+const num_get = 10;
+
+// XMLHttpRequest 객체 생성
 var xhr = new XMLHttpRequest();
+
+// 요청보낼 서버의 url 작성
 var url = 'http://apis.data.go.kr/B552555/lhNoticeInfo/getNoticeInfo'; /*URL*/
 var queryParams = '?' + encodeURIComponent('ServiceKey') + '=' + 'QAhz0fVQ0EaHGSFwGEwVJSdulqxl0mvQ9kOoUrcZMUpvsWVTIHRFhAwsxzqCmKJwxjHtWEXwiAlCVTeP0KNgJQ%3D%3D'; /*Service Key*/
-queryParams += '&' + encodeURIComponent('PG_SZ') + '=' + encodeURIComponent('10'); /**/
+queryParams += '&' + encodeURIComponent('PG_SZ') + '=' + encodeURIComponent(num_get); /**/
 queryParams += '&' + encodeURIComponent('PAGE') + '=' + encodeURIComponent('1'); /**/
 queryParams += '&' + encodeURIComponent('SCH_ST_DT') + '=' + encodeURIComponent('2020-01-01'); /**/
 queryParams += '&' + encodeURIComponent('SCH_ED_DT') + '=' + encodeURIComponent(today_date); /**/
@@ -15,27 +23,27 @@ queryParams += '&' + encodeURIComponent('AIS_TP_CD_INT') + '=' + encodeURICompon
 queryParams += '&' + encodeURIComponent('AIS_TP_CD_INT2') + '=' + encodeURIComponent(''); /**/
 queryParams += '&' + encodeURIComponent('AIS_TP_CD_INT3') + '=' + encodeURIComponent(''); /**/
 
-xhr.open('GET', url + queryParams);
+// 서버로 보낸 요청에 대한 응답을 받았을 때 수행할 함수
 xhr.onreadystatechange = function () {
+    // 4 == XMLHttpRequest.DONE
     if (this.readyState == 4) {
-        alert('Status: ' + this.status + 'nHeaders: ' + JSON.stringify(this.getAllResponseHeaders()) + 'nBody: ' + this.responseText);
         var myArray = this.responseText;
-        console.log(this.responseText);
-        console.log(myArray);
-        buildTable(myArray);
+        var obj = JSON.parse(myArray);
+        buildTable(obj);
     }
 };
 
+// 서버에 요청 전송
+xhr.open('GET', url + queryParams);
 xhr.send('');
-console.log(xhr);
-console.log(xhr.response);
 
+// 테이블 작성 함수
 function buildTable(data) {
     var table = document.getElementById('table1');
 
-    for (var i = 0; i < data[1].dsList[1].ALL_CNT; i++) {
+    for (var i = 0; i < num_get; i++) {
         var row = `<tr>
-                    <td>${i}</td>
+                    <td>${i + 1}</td>
                     <td>${data[1].dsList[i].BBS_WOU_DTTM}</td>
                     <td>${data[1].dsList[i].AIS_TP_CD_NM}</td>
                     <td>${data[1].dsList[i].BBS_TL}</td>
